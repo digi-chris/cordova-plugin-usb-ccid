a7c720e14a88488a962ba0a20a4179ef15af9034
ChrisB <chris.barnard@btinternet.com>
2019-02-20 16:54:28 +0000

    Initial commit

---
+++ www/nfc.js
@@ -0,0 +1,21 @@
+var exec = require('cordova/exec');
+/**
+ * @name KUsbNfc
+ * @description This plugin is responsible to communicate with ACS ACR122U NFC reader device
+ * @author Krishnendu Sekhar Das
+ */
+function KUsbNfc() { }
+
+KUsbNfc.prototype.connect = function (successCallback, errorCallback) {
+    exec(successCallback, errorCallback, 'KUsbNfc', 'connect', []);
+};
+
+KUsbNfc.prototype.listen = function (successCallback, errorCallback) {
+    exec(successCallback, errorCallback, 'KUsbNfc', 'listen', []);
+};
+
+KUsbNfc.prototype.disconnect = function (successCallback, errorCallback) {
+    exec(successCallback, errorCallback, 'KUsbNfc', 'disconnect', []);
+};
+
+module.exports = new KUsbNfc();
